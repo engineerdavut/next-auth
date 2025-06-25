090bc8366b6e7dae858fe5a4189e0044386275d8
engineerdavut <engineerdavut@gmail.com>
2025-06-25 11:33:54 +0300

    file directories organized.

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-           <div className="p-10 bg-white rounded-lg shadow-xl text-center">
+    <main className="flex flex-col items-center justify-center min-h-screen p-24 bg-gray-100">
+      <div className="p-10 bg-white rounded-lg shadow-xl text-center">
         <h1 className="text-3xl font-bold mb-4">Next.js & Auth0 Integration</h1>
         <p className="mb-6 text-gray-600">
           This is the home page. Anyone can see it.
@@ -17,54 +17,6 @@ export default function Home() {
           Protected Profile Page
         </Link>
       </div>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-        <AuthButtons />
-      </footer>
-    </div>
+    </main>
   );
 }
